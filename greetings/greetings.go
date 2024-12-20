10d31fea2a1f043e92260b2423597ba5b43c8f10
jewelsonmyjeans <aviadchuraman@gmail.com>
2024-12-20 18:15:47 -0500

    Made the greeting better

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,15 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
-func Hello(name string, age int, location string) string {
+func Hello() string {
 
-	message := fmt.Sprintf("Greetings, %v! You are %v years old and located in %v", name, age, location)
+	var age int
+
+	fmt.Print("Enter full name: ")
+	reader := bufio.NewReader(os.Stdin)
+	name, _ := reader.ReadString('\n')
+	name = name[:len(name)-1]
+
+	fmt.Print("Enter your age: ")
+	fmt.Scan(&age)
+
+	fmt.Print("Enter your location: ")
+	location, _ := reader.ReadString('\n')
+	location = location[:len(location)-1]
+
+	message := fmt.Sprintf("Greetings %s! You are %d years old and located in %s", name, age, location)
 	fmt.Println(message)
+
 	return message
 
 }
 
 func main() {
-	Hello("Aviad", 21, "Florida")
+	Hello()
 }
